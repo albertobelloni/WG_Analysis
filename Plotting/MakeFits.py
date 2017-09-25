544bedb3b6cb32c7d1386f7dc48a11c5607e6741
Josh Kunkle <jkunkle@cern.ch>
2017-09-25 12:24:09 -0400

    make first optmization plot

--- Plotting/MakeFits.py
+++ Plotting/MakeFits.py
@@ -1,14 +1,19 @@
 import ROOT
+import uuid
+import os
+import subprocess
 ROOT.PyConfig.IgnoreCommandLineOptions = True
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
 
 parser.add_argument( '--baseDir',  dest='baseDir', required=True, help='path to workspace directory' )
-parser.add_argument( '--outputCard',  dest='outputCard', required=False, help='name of output card' )
+parser.add_argument( '--outputDir',  dest='outputDir', required=False, default=None, help='name of output diretory for cards' )
 parser.add_argument( '--doStatTests',  dest='doStatTests', default=False, action='store_true', help='run statistical tests of WGamma background' )
 parser.add_argument( '--doWJetsTests',  dest='doWJetsTests', default=False, action='store_true', help='run tests of Wjets background' )
 parser.add_argument( '--doFinalFit',  dest='doFinalFit', default=False, action='store_true', help='run fit to data and background' )
+parser.add_argument( '--doToyDataFit',  dest='doToyDataFit', default=False, action='store_true', help='run fit to toy data and background' )
+parser.add_argument( '--combineDir',  dest='combineDir', default=None, help='path to combine directory' )
 
 options = parser.parse_args()
 
@@ -21,18 +26,127 @@ def main() :
     key_wjets  = 'workspace_wjets'
     key_data   = 'workspace_data'
 
-    #workspace_toy = ROOT.RooWorkspace( 'workspace_toy' )
-    #generate_toy_data( options.baseDir, key_wgamma, 'dijet_Wgamma_mu_EB', workspace_toy, suffix='mu_EB' )
-    #workspace_toy.writeToFile( '%s/%s.root' %( options.baseDir, workspace_toy.GetName() ) )
+
+    #bins = ['mu_EB', 'mu_EE', 'el_EB', 'el_EE']
+    bins = [
+        {'channel' : 'mu', 'eta' : 'EB' },
+    ]
+
+    if options.outputDir is not None :
+        if not os.path.isdir( options.outputDir ) :
+            os.makedirs( options.outputDir )
 
 
-    signal_pred = 'srhistpdf_MadGraphResonanceMass300_width0p01_mu_EB'
-    #signal_pred = 'signal'
+    #signal_points = [200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800, 2000, 2200, 2400, 2600, 2800, 3000, 3500, 4000]
+    signal_points = [200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1600, 1800]
+    #signal_points = [200, 250, 300, 350, 400, 450, 500, 600, 700, 800]
 
     #generate_card( options.baseDir, 'workspace_toy', 'workspace_signal', signal_pred, options.outputCard )
+    if options.doToyDataFit :
+
+        kine_vars = [ { 'name' : 'mt_incl_lepph_z', 'color' : ROOT.kBlue},
+                     { 'name' : 'm_incl_lepph_z' , 'color' : ROOT.kRed },
+                     { 'name' : 'mt_fulltrans'   , 'color' : ROOT.kBlack },
+                     { 'name' : 'mt_constrwmass' , 'color' : ROOT.kGreen },
+                     { 'name' : 'ph_pt'          , 'color' : ROOT.kMagenta },
+                   ]
+
+
+        workspace_toy = ROOT.RooWorkspace( 'workspace_toy' )
+
+        for var in kine_vars :
+            wgamma_entry = get_workspace_entry( 'wgamma', 'mu', 'EB', var['name'] )
+            wjets_entry  = get_workspace_entry( 'wjets', 'mu', 'EB', var['name'] )
+
+            #generate_toy_data( options.baseDir, [key_wgamma, key_wjets], [wgamma_entry, wjets_entry], workspace_toy, suffix='mu_EB' )
+            generate_toy_data( options.baseDir, [key_wgamma], [wgamma_entry], workspace_toy, suffix='mu_%s_EB' %var['name'] )
+
+
+        workspace_toy.writeToFile( '%s/%s.root' %( options.baseDir, workspace_toy.GetName() ) )
+
+        all_cards = {}
+
+        for var in kine_vars :
+            for sig_pt in signal_points :
+                signal_dic = { 'name' : 'Resonance', 
+                               'path' : '%s/%s.root' %( options.baseDir, key_signal ), 
+                               'wsname' : key_signal, 
+                              'hist_base' : 'srhistpdf_MadGraphResonanceMass%d_width0p01' %sig_pt }
+
+                data_dic = { 'path' : '%s/workspace_toy.root' %( options.baseDir), 
+                             'wsname' : 'workspace_toy', 
+                             'hist_base' : 'toydata' }
+
+                backgrounds = [ 
+                               {'name' : 'Wgamma' , 'path' : '%s/%s.root' %( options.baseDir, key_wgamma ), 'wsname' : key_wgamma, 'hist_base' : 'wgamma' }
+                              ]
+                card_path = options.outputDir + 'wgamma_test_%s_%d.txt' %(var['name'], sig_pt )
+
+                generate_card( options.baseDir, data_dic, signal_dic, backgrounds, bins, var['name'], card_path  )
+
+                all_cards.setdefault(var['name'], {})
+                all_cards[var['name']][sig_pt] =  card_path 
+
+        if options.combineDir is not None :
+            ofile = open( '%s/run_combine.sh' %(options.outputDir), 'w' )
+            ofile.write( '#!/bin/tcsh\n' )
+            ofile.write( 'cd %s \n' %options.combineDir ) 
+            ofile.write( 'eval `scramv1 runtime -csh` \n' ) 
+            output_files = {}
+            for var, ptdic in all_cards.iteritems() :
+                for pt, card in ptdic.iteritems() :
+                    log_file = '%s/results_%s_%d.txt'%( options.outputDir, var, pt )
+                    ofile.write( 'combine -M Asymptotic -m 1000 -t 1 %s >> %s \n'  %( card, log_file ))
+                    output_files.setdefault(var, {} )
+                    output_files[var][pt] = log_file
+
+            ofile.write( ' cd - \n' )
+            ofile.write( 'echo "^.^ FINISHED ^.^" \n' )
+            
+            os.system( 'chmod 777 %s/run_combine.sh' %(options.outputDir) )
+
+            ofile.close()
+
+            #subprocess.Popen( '%s/run_combine.sh' %( options.outputDir ), shell=True  )
+            #os.system( '%s/run_combine.sh' %( options.outputDir ) )
+
+            combine_results = {}
+            for var, ptdic in output_files.iteritems() :
+                for pt, f in ptdic.iteritems() :
+                    result = process_combine_file( f )
+                    combine_results.setdefault(var, {})
+                    combine_results[var][pt] = float( result['Observed Limit'].split('<')[1] )
+
+            limit_graphs = []
+            for var in kine_vars :
+
+                npoints = len( combine_results[var['name']] )
+                limit_graph = ROOT.TGraph( npoints )
+                limit_graph.SetName( 'graph_%s' %var['name'] )
+                limit_graph.SetLineColor( var['color'] )
+
+                for idx, pt in enumerate(signal_points) :
+                    limit_graph.SetPoint( idx, pt, combine_results[var['name']][pt])
+
+                limit_graphs.append( limit_graph )
+
+            can = ROOT.TCanvas( 'limit_result' ,'Limit result' )
+            for idx, graph in enumerate(limit_graphs) :
+                if idx == 0 :
+                    graph.Draw('AL')
+                else :
+                    graph.Draw('Lsame')
+
+        
+            raw_input('cont')
+
+
+
+
+
 
     if options.doStatTests :
-        run_statistical_tests( options.baseDir, key_wgamma, 'dijet_Wgamma_mu_EB', 100, options.baseDir, suffix='mu_EB' )
+        run_statistical_tests( options.baseDir, key_wgamma, 'dijet_wgamma_mu_EB', 100, options.baseDir, suffix='mu_EB' )
 
     if options.doFinalFit :
         generate_card( options.baseDir, key_data, etc )
@@ -47,6 +161,33 @@ def main() :
     #make_gauss_signal( options.baseDir, workspace_toysignal )
     #workspace_toysignal.writeToFile( '%s/%s.root' %( options.baseDir, workspace_toysignal.GetName() ) )
     #generate_card( options.baseDir, 'workspace_toy', 'workspace_toysignal', signal_pred, options.outputCard )
+
+def get_workspace_entry( process, channel, eta, var ) : 
+
+    if process == 'wjets' :
+        return 'dijet_prediction_%s_%s_%s_%s' %( process, channel, var, eta )
+    elif process == 'toydata' :
+        return '%s_%s_%s_%s' %( process,channel, var, eta )
+    elif process.count('ResonanceMass') :
+        return '%s_%s_%s_%s' %( process,channel, var, eta )
+    else :
+        return 'dijet_%s_%s_%s_%s' %( process,channel, var, eta )
+
+def process_combine_file( fname ) :
+
+    ofile = open( fname, 'r' )
+
+    results = {}
+
+    for line in ofile :
+        if line.count(':') > 0 :
+            spline = line.split(':')
+            results[spline[0]] = spline[1].rstrip('\n')
+
+    ofile.close()
+
+    return results
+
 
 def run_bkg_scale_test( baseDir, const_ws, const_hist, var_ws, var_hist ) :
 
@@ -170,16 +311,9 @@ def make_fit_workspace( baseDir, workspace_key ) :
         print pdf_list[i]
 
 
-def generate_card( baseDir, data_workspace, signal_workspace, signal_pred, outputCard ) :
+def generate_card( baseDir, data_info, signal_info, backgrounds, bins, kine_var, outputCard ) :
 
     card_entries = []
-
-
-    #bins = ['mu_EB', 'mu_EE', 'el_EB', 'el_EE']
-    bins = ['mu_EB']
-    backgrounds = [ 
-        {'name' : 'Wgamma' , 'path' : '%s/workspace_wgamma.root' %( baseDir ), 'wsname' : 'workspace_wgamma' }
-    ]
 
     section_divider = '-'*100
 
@@ -192,21 +326,29 @@ def generate_card( baseDir, data_workspace, signal_workspace, signal_pred, outpu
     max_name_len = max( [len(x['name']) for x in backgrounds ] )
     max_path_len = max( [len(x['path']) for x in backgrounds ] )
 
+    all_binids = []
     for ibin in bins :
+        bin_id = '%s_%s' %( ibin['channel'], ibin['eta'] )
+        all_binids.append(bin_id)
+
         for bkgdic in backgrounds :
-            card_entries.append( 'shapes %s %s %s %s:dijet_%s_%s' %( bkgdic['name'].ljust( max_name_len ), ibin, bkgdic['path'].ljust( max_path_len ), bkgdic['wsname'], bkgdic['name'], ibin ) )
-        card_entries.append( 'shapes Resonance %s %s/%s.root %s:%s' %( ibin, baseDir, signal_workspace, signal_workspace, signal_pred) )
-        card_entries.append( 'shapes data_obs %s %s/%s.root %s:toydata%s' %( ibin, baseDir, data_workspace, data_workspace, ibin ) )
+            bkg_entry = get_workspace_entry( bkgdic['hist_base'], ibin['channel'], ibin['eta'], kine_var )
+            card_entries.append( 'shapes %s %s %s %s:%s' %( bkgdic['name'].ljust( max_name_len ), bin_id, bkgdic['path'].ljust( max_path_len ), bkgdic['wsname'], bkg_entry ) )
+
+        signal_entry = get_workspace_entry( signal_info['hist_base'], ibin['channel'], ibin['eta'], kine_var )
+        data_entry = get_workspace_entry( data_info['hist_base'], ibin['channel'], ibin['eta'], kine_var )
+        card_entries.append( 'shapes %s %s %s %s:%s' %( signal_info['name'], bin_id, signal_info['path'], signal_info['wsname'], signal_entry ) )
+        card_entries.append( 'shapes data_obs %s %s %s:%s' %( bin_id, data_info['path'], data_info['wsname'], data_entry ) )
 
     card_entries.append( section_divider )
 
-    card_entries.append( 'bin          ' + '    '.join( bins ) )
+    card_entries.append( 'bin          ' + '    '.join( all_binids ) )
     card_entries.append( 'observation  ' + '    '.join( ['-1.0']*len(bins) ) )
 
     card_entries.append( section_divider )
 
-    card_entries.append( 'bin      ' + '    '.join(['    '.join( [x for x in bins] )]*( len(backgrounds) + 1 ) ) ) 
-    card_entries.append( 'process  ' + '    '.join( ( ['Resonance'] + [x['name'] for x in backgrounds])*len(bins) ) )
+    card_entries.append( 'bin      ' + '    '.join(['    '.join( [x for x in all_binids] )]*( len(backgrounds) + 1 ) ) ) 
+    card_entries.append( 'process  ' + '    '.join( ( [signal_info['name']] + [x['name'] for x in backgrounds])*len(bins) ) )
     card_entries.append( 'process  ' + '    '.join( [str(x) for x in range(0, len(backgrounds) +1 ) ]*len(bins) ) )
     card_entries.append( 'rate     ' + '    '.join( ['1.0','1.0' ]*len(bins) ) )
 
@@ -231,21 +373,52 @@ def generate_card( baseDir, data_workspace, signal_workspace, signal_pred, outpu
             print ent
 
 
-def generate_toy_data( basedir, ws_key, hist_key, out_ws, suffix='' ) :
+def generate_toy_data( basedir, ws_key_list, hist_key_list, out_ws, suffix='' ) :
 
-    ofile = ROOT.TFile.Open( '%s/%s.root' %( basedir, ws_key ) )
+    if not isinstance( ws_key_list, list ) :
+        ws_key_list = [ws_key_list]
+    if not isinstance( hist_key_list, list ) :
+        hist_key_list = [hist_key_list]
 
-    ws = ofile.Get( ws_key )
+    pdfs = []
+    norms = []
+    xvar = None
+    for ws_key, hist_key in zip( ws_key_list, hist_key_list ) :
 
-    normalization = ws.var( '%s_norm' %hist_key )
-    pdf = ws.pdf( hist_key )
-    xvar = ws.var('x')
+        ofile = ROOT.TFile.Open( '%s/%s.root' %( basedir, ws_key ) )
 
-    dataset = pdf.generate( ROOT.RooArgSet(xvar), int(normalization.getValV()), ROOT.RooCmdArg( 'Name', 0, 0, 0, 0, 'toydata%s' %suffix ) )
+        ws = ofile.Get( ws_key )
+
+        norms.append(ws.var( '%s_norm' %hist_key ))
+        pdfs.append(ws.pdf( hist_key ))
+
+        if xvar is None :
+            xvar = ws.var('x')
+
+        ofile.Close()
+
+    if len( pdfs ) == 1 :
+        dataset = pdfs[0].generate( ROOT.RooArgSet(xvar), int(norms[0].getValV()), ROOT.RooCmdArg( 'Name', 0, 0, 0, 0, 'toydata_%s' %suffix ) )
+    else :
+        total = sum( [x.getValV() for x in norms ] )
+
+        fractions = [ x.getValV()/total for x in norms ] 
+
+        pdfList = ROOT.RooArgList()
+        for p in pdfs :
+            pdfList.add( p )
+
+        fracList = ROOT.RooArgList()
+        for f in fractions[:-1] :
+            myvar = ROOT.RooRealVar( str(uuid.uuid4()), str(uuid.uuid4()), f )
+            myvar.Print()
+            fracList.add( myvar )
+
+        summed = ROOT.RooAddPdf( 'summed' ,'summed', pdfList , fracList )
+        dataset = summed.generate( ROOT.RooArgSet(xvar), int(total), ROOT.RooCmdArg( 'Name', 0, 0, 0, 0, 'toydata_%s' %suffix ) )
 
     # not clear why we have to rename here
-    getattr( out_ws, 'import' ) ( dataset, ROOT.RooCmdArg('RenameAllNodes', 0, 0, 0, 0, 'toydata%s' %suffix ) )
-
+    getattr( out_ws, 'import' ) ( dataset, ROOT.RooCmdArg('RenameAllNodes', 0, 0, 0, 0, 'toydata_%s' %suffix ) )
 
 def run_statistical_tests( basedir, ws_key, hist_key, niter=100, outputDir=None, suffix='' ) :
 
