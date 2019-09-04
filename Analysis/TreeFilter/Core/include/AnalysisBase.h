700d13ba8753f413652b82895a3f60a9a4f45861
sciencecw <sciencecw@gmail.com>
2019-09-04 13:10:22 -0400

    copy and merge histograms in TreeFilter nutples

--- Analysis/TreeFilter/Core/include/AnalysisBase.h
+++ Analysis/TreeFilter/Core/include/AnalysisBase.h
@@ -249,6 +249,7 @@ void ParseHistPars( const std::string & line, ModuleConfig& module);
 void ParseDataEntry( const std::string & line, ModuleConfig& module);
 void ReadHeaderLine( const std::string & line, CmdOptions & options);
 void ParseFiles( const std::string & line, CmdOptions& options);
+void MergeRootfile( TDirectory *target, TList *sourcelist );
 
 std::vector<std::string> Tokenize(const std::string & input, const std::string &sep );
 
