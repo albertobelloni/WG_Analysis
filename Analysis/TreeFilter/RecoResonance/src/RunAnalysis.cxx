ef361dacc5d6b827c7c5aee408d168211990a7c0
Yihui Lai <yihui.lai@cern.ch>
2022-06-12 10:45:49 -0400

    check JETID numer again

--- Analysis/TreeFilter/RecoResonance/src/RunAnalysis.cxx
+++ Analysis/TreeFilter/RecoResonance/src/RunAnalysis.cxx
@@ -2634,26 +2634,19 @@ void RunModule::FilterJet( ModuleConfig & config ) const {
                 if( fabs( eta ) > 2.6 ) {
                     if( !config.PassFloat( "cut_jet_nhf_central2_tight" , nhf  ) ) pass_tight = false;
                     if( !config.PassFloat( "cut_jet_nemf_central2_tight", nemf ) ) pass_tight = false;
-                    if( !config.PassInt  ( "cut_jet_nconst_central2_tight", nconst ) ) pass_tight = false;
+                    if( !config.PassInt( "cut_jet_cmult_central2_tight" , cmult  ) ) pass_tight = false;
 
-                    if( !config.PassFloat( "cut_jet_nhf_central2_tightlep" , nhf  ) ) pass_tightlep = false;
-                    if( !config.PassFloat( "cut_jet_nemf_central2_tightlep", nemf ) ) pass_tightlep = false;
-                    if( !config.PassInt  ( "cut_jet_nconst_central2_tightlep", nconst ) ) pass_tightlep = false;
                     if( !config.PassFloat  ( "cut_jet_muf_central2_tightlep", muf ) ) pass_tightlep = false;
+                    if( !config.PassFloat( "cut_jet_cemf_central_tightlep" , cemf ) ) pass_tightlep = false;
                 }else{
-
-                    if( !config.PassFloat( "cut_jet_nhf_central_loose" , nhf  ) ) pass_loose = false;
-                    if( !config.PassFloat( "cut_jet_nemf_central_loose", nemf ) ) pass_loose = false;
-                    if( !config.PassInt  ( "cut_jet_nconst_central_loose", nconst ) ) pass_loose = false;
-    
                     if( !config.PassFloat( "cut_jet_nhf_central_tight" , nhf  ) ) pass_tight = false;
                     if( !config.PassFloat( "cut_jet_nemf_central_tight", nemf ) ) pass_tight = false;
                     if( !config.PassInt  ( "cut_jet_nconst_central_tight", nconst ) ) pass_tight = false;
+                    if( !config.PassFloat( "cut_jet_chf_central_tight" , chf  ) ) pass_tight = false;
+                    if( !config.PassInt( "cut_jet_cmult_central_tight" , cmult  ) ) pass_tight = false;
     
-                    if( !config.PassFloat( "cut_jet_nhf_central_tightlep" , nhf  ) ) pass_tightlep = false;
-                    if( !config.PassFloat( "cut_jet_nemf_central_tightlep", nemf ) ) pass_tightlep = false;
-                    if( !config.PassInt  ( "cut_jet_nconst_central_tightlep", nconst ) ) pass_tightlep = false;
                     if( !config.PassFloat  ( "cut_jet_muf_central_tightlep", muf ) ) pass_tightlep = false;
+                    if( !config.PassFloat( "cut_jet_cemf_central_tightlep" , cemf ) ) pass_tightlep = false;
                 }
             }
         }
@@ -2672,9 +2665,9 @@ void RunModule::FilterJet( ModuleConfig & config ) const {
             if( !config.PassFloat( "cut_jet_nemf_forward_loose", nemf ) ) pass_loose = false;
             if( !config.PassInt( "cut_jet_nmult_forward_loose", nmult ) ) pass_loose = false;
 
+            if( !config.PassFloat( "cut_jet_nhf_forward_tight", nhf ) ) pass_tight = false;
             if( !config.PassFloat( "cut_jet_nemf_forward_tight", nemf ) ) pass_tight = false;
             if( !config.PassInt( "cut_jet_nmult_forward_tight", nmult ) ) pass_tight = false;
-            if( !config.PassInt( "cut_jet_nhf_forward_tight", nhf ) ) pass_tight = false;
         }
 
         if( !config.PassBool( "cut_tight", pass_tight) ) continue; 
